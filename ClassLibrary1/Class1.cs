1518d9c7b7ea3129a66c6cb1c84e773212506e4f
ragun <ragun@DESKTOP-U88F0FF>
2021-09-11 20:17:25 -0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿public class Employee
+{
+    public int empID;
+    public string empName;
+    public double salaryPerHour;
+    public double noOfWorkingHours;
+    public double netSalary;
+
+    public static string organizationName = "Harsha Pvt. Ltd";
+    public const string typeOfEmployee = "Contract Based";
+    public readonly string departmentName = "Finance Department";
+
+}
